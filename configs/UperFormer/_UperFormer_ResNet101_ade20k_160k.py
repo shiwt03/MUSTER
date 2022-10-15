0dfd943e1151f1f69e0be55ce30850700b38191b
shiwt03 <shiwt2017@outlook.com>
2022-10-15 19:32:21 +0800

    Add Upernet with ResNet Backbone

---
+++ configs/UperFormer/_UperFormer_ResNet101_ade20k_160k.py
@@ -0,0 +1,96 @@
+# model settings
+
+_base_ = [
+    '../_base_/datasets/ade20k.py',
+    '../_base_/default_runtime.py',
+    '../_base_/schedules/schedule_160k.py'
+]
+checkpoint_file = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/swin/swin_base_patch4_window12_384_22k_20220317-e5c09f74.pth'  # noqa
+norm_cfg = dict(type='LN', requires_grad=True)
+backbone_norm_cfg = dict(type='SyncBN', requires_grad=True)
+model = dict(
+    type='EncoderDecoder',
+    pretrained='open-mmlab://resnet101_v1c',
+    backbone=dict(
+        type='ResNetV1c',
+        depth=101,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        dilations=(1, 1, 1, 1),
+        strides=(1, 2, 2, 2),
+        norm_cfg=backbone_norm_cfg,
+        norm_eval=False,
+        style='pytorch',
+        contract_dilation=True),
+    decode_head=dict(
+        type='UperFormerHead',
+        embed_dims=2048,
+        patch_size=4,
+        window_size=12,
+        mlp_ratio=4,
+        depths=(2, 2, 2, 2),
+        num_heads=(32, 16, 8, 4),
+        strides=(2, 2, 2, 4),
+
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.,
+        attn_drop_rate=0.,
+        drop_path_rate=0.1,
+        act_cfg=dict(type='GELU'),
+        # norm_cfg=dict(type='LN'),
+        with_cp=False,
+        init_cfg=None,
+        in_channels=[2048, 1024, 512, 256],
+        in_index=[0, 1, 2, 3],
+        # pool_scales=(1, 2, 3, 6),
+        channels=512,
+        # dropout_ratio=0.1,
+        num_classes=150,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    auxiliary_head=dict(
+        type='FCNHead',
+        in_channels=1024,
+        in_index=2,
+        channels=256,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=0.1,
+        num_classes=150,
+        norm_cfg=dict(type='SyncBN', requires_grad=True),
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
+
+
+optimizer = dict(
+    _delete_=True,
+    type='AdamW',
+    lr=0.00006,
+    betas=(0.9, 0.999),
+    weight_decay=0.01,
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'relative_position_bias_table': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.)
+        }))
+
+lr_config = dict(
+    _delete_=True,
+    policy='poly',
+    warmup='linear',
+    warmup_iters=1500,
+    warmup_ratio=1e-6,
+    power=1.0,
+    min_lr=0.0,
+    by_epoch=False)
+
+# By default, models are trained on 8 GPUs with 2 images per GPU
+data = dict(samples_per_gpu=4)
