fad97c8b92b4673927700838e5585ab183c5c5c4
shiwt03 <shiwt2017@outlook.com>
2022-10-27 11:17:30 +0800

    Add Ablation Experiment Code on Fuse&Upsample

--- configs/UperFormer/_UperFormer_swin-base_Deconv_ade20k_160k.py
+++ configs/UperFormer/_UperFormer_swin-base_Deconv_ade20k_160k.py
@@ -1,6 +1,10 @@
 # model settings
 
-checkpoint_file = None  # noqa
+_base_ = [
+    '../_base_/models/UperFormer_swin.py', '../_base_/datasets/ade20k.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
+]
+checkpoint_file = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/swin/swin_base_patch4_window12_384_22k_20220317-e5c09f74.pth'  # noqa
 norm_cfg = dict(type='LN', requires_grad=True)
 backbone_norm_cfg = dict(type='LN', requires_grad=True)
 model = dict(
@@ -28,7 +32,7 @@ model = dict(
         act_cfg=dict(type='GELU'),
         norm_cfg=backbone_norm_cfg),
     decode_head=dict(
-        type='UperFormerHead',
+        type='UperFormerHead_Deconv',
         embed_dims=1024,
         patch_size=4,
         window_size=12,
@@ -59,3 +63,30 @@ model = dict(
     # model training and testing settings
     train_cfg=dict(),
     test_cfg=dict(mode='whole'))
+
+
+optimizer = dict(
+    _delete_=True,
+    type='AdamW',
+    lr=0.00006,
+    betas=(0.9, 0.999),
+    weight_decay=0.01,
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'relative_position_bias_table': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.)
+        }))
+
+lr_config = dict(
+    _delete_=True,
+    policy='poly',
+    warmup='linear',
+    warmup_iters=1500,
+    warmup_ratio=1e-6,
+    power=1.0,
+    min_lr=0.0,
+    by_epoch=False)
+
+# By default, models are trained on 8 GPUs with 2 images per GPU
+data = dict(samples_per_gpu=1)
